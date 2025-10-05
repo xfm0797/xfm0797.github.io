59f4cd3e24a1aae8760bdefce7fd78fcd8157251
xfm0797 <lovouster@gmail.com>
2025-10-05 17:11:31 +0800

    Site updated: 2025-10-05 17:11:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/05/hexo-setup/","2025/10/05/upload/","2025/10/05/provider/","2025/10/05/vercel/","2025/10/05/questions/","2025/10/05/qexo-bulid/","2025/10/05/hello-world/","2025/10/05/qexo/","2025/10/05/hexo-theme-Butterfly/"];function toRandomPost(){
+var posts=["2025/10/05/hexo-setup/","2025/10/05/upload/","2025/10/05/provider/","2025/10/05/vercel/","2025/10/05/questions/","2025/10/05/qexo-bulid/","2025/10/05/hello-world/","2025/10/05/qexo/","2025/10/05/hexo-theme-anzhiyu-install/","2025/10/05/hexo-theme-Butterfly/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
